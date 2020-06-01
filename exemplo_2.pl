d92560367f312c1663efb7d8f031e4f85fd5f313
Rafael <rafael.merlomendes@gmail.com>
2020-06-01 15:15:51 -0300

    Adicionado o 2º exemplo de Prolog.

---
+++ exemplo_2.pl
@@ -0,0 +1,15 @@
+% Fatos
+gerou(kelly, maria).
+gerou(cleber, maria).
+gerou(cleber, bruna).
+gerou(maria, julia).
+gerou(maria, pedro).
+gerou(pedro, lucas).
+
+feminino(kelly).
+feminino(maria).
+feminino(julia).
+feminino(bruna).
+masculino(cleber).
+masculino(pedro).
+masculino(lucas).

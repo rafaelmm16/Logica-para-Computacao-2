bae347b2996a3b32412caffab574cd035205c5fa
Rafael <rafael.merlomendes@gmail.com>
2020-06-21 23:49:41 -0300

    Adicionado mais uma parte do código.

--- pokemon.pl
+++ pokemon.pl
@@ -17,9 +17,27 @@ ataque("charmander", 25).
 ataque("squirtle", 25).
 ataque("bulbasaur", 25).
 
+ataque("charmeleon", 45).
+ataque("wartortle", 45).
+ataque("ivysaur", 45).
+
+ataque("charizard", 75).
+ataque("blastoise", 75).
+ataque("venosaur", 75).
+
+
 defesa("charmander", 15).
 defesa("squirtle", 15).
 defesa("bulbasaur", 15).
+
+defesa("charmeleon", 35).
+defesa("wartortle", 35).
+defesa("ivysaur", 35).
+
+defesa("charizard", 65).
+defesa("blastoise", 65).
+defesa("venosaur", 65).
+
 
 
 %	Regras
@@ -34,7 +52,17 @@ forma_final(Level,Pokemon):-
     evolucao(Pokemon),
     write("Pode evoluir.").
 
-status(Pokemon,Ataque,Defesa):-
+%       Status
+status_base(Pokemon,Ataque,Defesa):-
     inicial(Pokemon),
     ataque(Pokemon,Ataque),
     defesa(Pokemon,Defesa).
+status_evolucao(Pokemon,Ataque,Defesa):-
+    evolucao(Pokemon),
+    ataque(Pokemon,Ataque),
+    defesa(Pokemon,Defesa).
+status_final(Pokemon,Ataque,Defesa):-
+    final(Pokemon),
+    ataque(Pokemon,Ataque),
+    defesa(Pokemon,Defesa).
+
